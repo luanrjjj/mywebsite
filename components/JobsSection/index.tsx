60244ffb1ac47c5ae15d53bd33573e37c3db7f60
Luan Alfredo Freitas Dias <luanfreitas12@poli.ufrj.br>
2021-10-10 02:43:26 -0300

    Jobs Section Construction

--- components/JobsSection/index.tsx
+++ components/JobsSection/index.tsx
@@ -1,16 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { FiGithub } from 'react-icons/fi'
 import { AiOutlineLinkedin } from 'react-icons/ai'
-import { Section, SectionContent } from './styles'
+import {
+  JobsList,
+  Section,
+  SectionContent,
+  SelectedJob,
+  Text,
+} from './styles'
 
 export const JobsSection = () => {
+  const [filter, setFilter] = useState('RioGaleão')
+
+  const filterJobs = ['RioGaleão', 'Lemobs']
   return (
     <Section>
-      <h2>Experiência Profissional</h2>
       <SectionContent>
-        <button>RioGaleão</button>
-        <button>Lemobs</button>
+        <h2>Experiência Profissional</h2>
+        <div className="FiltersAndText">
+          <JobsList>
+            {filterJobs?.map((Job: string) => {
+              return (
+                <SelectedJob
+                  onClick={() => setFilter(Job)}
+                  selected={Job === filter}
+                  key={Job}
+                >
+                  <a>{Job}</a>
+                </SelectedJob>
+              )
+            })}
+          </JobsList>
+          <Text>
+            {filter === 'RioGaleão' ? (
+              <>
+                <span className="Function">
+                  Estágio
+                  <a className="Company">
+                    {' '}
+                    @ Aeroporto RioGaleão
+                  </a>
+                </span>
+                <ul>
+                  <li>
+                    Lorem ipsum fermentum taciti aliquam
+                    elementum hac malesuada nullam quis in
+                    porttitor, lorem facilisis commodo
+                    suspendisse felis aliquam venenatis hac
+                    imperdiet tristique sem, sit vitae
+                    curabitur elit cras massa sem lectus
+                    quis curabitur.
+                  </li>
+                </ul>
+              </>
+            ) : (
+              <>
+                <span>Estágio</span>
+                <p>
+                  Lorem ipsum fermentum taciti aliquam
+                  elementum hac malesuada nullam quis in
+                </p>
+                <p>
+                  Lorem ipsum fermentum taciti aliquam
+                  elementum hac malesuada nullam quis in
+                </p>
+              </>
+            )}
+          </Text>
+        </div>
       </SectionContent>
     </Section>
   )
--- components/JobsSection/styles.ts
+++ components/JobsSection/styles.ts
@@ -1,21 +1,92 @@
 import styled from 'styled-components'
 import colors from '../../styles/colors'
 
+interface JobsProps {
+  selected: boolean
+  onClick: any
+}
 export const Section = styled.div`
+  max-width: 100%;
+`
+export const SectionContent = styled.div`
   max-width: 700px;
   margin: auto;
-`
-export const SectionContent = styled.div``
 
-export const JobsList = styled.div`
+  h2 {
+    counter-increment: item 1;
+    overflow: hidden;
+    position: relative;
+    color: ${colors.white};
+
+    &:before {
+      content: '02.';
+      margin-right: 5px;
+      color: #22543d;
+      text-align: right;
+    }
+    &:after {
+      content: '';
+      display: block;
+      position: relative;
+      top: -12px;
+      width: 300px;
+      height: 1px;
+      margin-left: 180px;
+    }
+  }
+
+  .FiltersAndText {
+    display: flex;
+  }
+`
+
+export const JobsList = styled.ul`
   position: relative;
   z-index: 3;
   width: max-content;
   padding: 0;
   margin: 0;
   list-style: none;
+
+  li {
+  }
 `
 
-export const JobsButton = styled.div`
-  
-  `
+export const SelectedJob = styled.li<JobsProps>`
+  padding: 20px;
+  background-color: ${props =>
+    props.selected ? `${colors.heavybody}` : ''};
+  color: ${props =>
+    props.selected ? `${colors.green300}` : '#fff'};
+  border-left: 3px solid
+    ${props =>
+      props.selected ? `${colors.green300}` : '#fff'};
+`
+export const Text = styled.div`
+  margin-left: 30px;
+
+  .Function {
+    color: ${colors.white};
+    margin-left:15px;
+  }
+  .Company {
+    color: ${colors.green300};
+
+    &:hover {
+    }
+  }
+
+  ul {
+    li:before {
+      content: '';
+      position: absolute;
+      border-right: 2px solid black;
+      border-bottom: 2px solid black;
+      width: 10px;
+      height: 10px;
+      top: calc(50% - 4px);
+      left: -20px;
+      transform: translateY(-50%) rotate(-45deg);
+    }
+  }
+`
