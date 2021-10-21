9a53a1ceb83ec3e4c6174919c7392d482004a2e7
Luan Alfredo Freitas Dias <luanfreitas12@poli.ufrj.br>
2021-10-21 17:38:32 -0300

    Adding google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,28 @@
+/* eslint-disable @next/next/inline-script-id */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import Script from 'next/script'
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-CRJE5KEGP4`}
+      />
+      // eslint-disable-next-line
+      @next/next/inline-script-id
+      <Script strategy="lazyOnload">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-CRJE5KEGP4');
+    `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
